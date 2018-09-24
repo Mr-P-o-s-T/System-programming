9f3cb7bc641353bc4bd4fe16d81d653e224ff38e
Александр <popovich601@gmail.com>
2018-09-24 16:02:30 +0300

    Stack corruption problem has been solved...

--- LAB.1.14/LAB.1.14/main.cpp
+++ LAB.1.14/LAB.1.14/main.cpp
@@ -10,20 +10,19 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	STARTUPINFO si[2];
 	PROCESS_INFORMATION pi[2];
 
-	ZeroMemory(&si, sizeof(si) * 2);
-	si[0].cb = sizeof(si[0]);
-	si[1].cb = sizeof(si[1]);
-	ZeroMemory(&pi, sizeof(pi) * 2);
+	ZeroMemory(&si, sizeof(si[0]) * 2);
+	si[0].cb = si[1].cb = sizeof(si[0]);
+	ZeroMemory(&pi, sizeof(pi[0]) * 2);
 
 	WCHAR a[256] = L"getF 0.0";
 
 	// Start the child process. 
-	if (!CreateProcess(L"..\\Debug\\function.exe", a, NULL, NULL, FALSE, 0, NULL, NULL, &si[0], &pi[0])) {
+	if (!CreateProcess(L"..\\x64\\Debug\\function.exe", a, NULL, NULL, FALSE, 0, NULL, NULL, &si[0], &pi[0])) {
 		DWORD error = GetLastError();
 		return NULL;
 	}
 
-	if (!CreateProcess(L"..\\Debug\\function.exe", a, NULL, NULL, FALSE, 0, NULL, NULL, &si[1], &pi[1])) {
+	if (!CreateProcess(L"..\\x64\\Debug\\function.exe", a, NULL, NULL, FALSE, 0, NULL, NULL, &si[1], &pi[1])) {
 		DWORD error = GetLastError();
 		return NULL;
 	}
